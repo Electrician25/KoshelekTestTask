be897974c99787ceba169a05ecc9ea2cc25b6997
Kirill <sosisks888@mail.ru>
2024-05-24 10:58:37 +0300

    add message history service

---
+++ KoshelekClient/KoshelekMessageHistoryClient/KoshelekMessageHistoryClient/Controller/HtmlController.cs
@@ -0,0 +1,24 @@
+﻿using KoshelekMessageHistoryClient.ActionResult;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KoshelekMessageHistoryClient.Controller
+{
+    [ApiController]
+    [Route("/api/{controller}")]
+    public class HtmlController : ControllerBase
+    {
+        private readonly Func<string, HtmlResult> _htmlResult;
+
+        public HtmlController(Func<string, HtmlResult> htmlResult)
+        {
+            _htmlResult = htmlResult;
+        }
+
+        [HttpGet]
+        [Route("GetHisoryMessage")]
+        public IActionResult GetHistoryMessage()
+        {
+            return _htmlResult(@"./wwwroot/html/MessageHistoryMainPage.html");
+        }
+    }
+}
